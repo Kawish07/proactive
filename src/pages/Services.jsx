27250c090d5affb3e3f5cdb1b7a964d2fec269a3
Kawish Iqbal <kawishiqbal898@gmail.com>
2025-08-26 08:56:57 -0700

    Update project files

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -15,14 +15,15 @@ const Services = () => {
           <nav className="px-8 py-8 flex items-center justify-between bg-[#02294D] backdrop-blur-sm rounded-t-3xl">
             <div className="flex items-center">
               <div className="flex items-center space-x-2">
-                <div className="w-8 h-8 bg-green-500 rounded-full flex items-center justify-center">
-                  <div className="w-5 h-5 bg-blue-600 rounded-full relative">
-                    <div className="absolute top-1 left-1 w-2 h-2 bg-green-400 rounded-full"></div>
-                  </div>
-                </div>
-                <div className="text-white">
+                <div className="flex items-center">
                   <Link to="/">
-                    <img src="https://proactivecleaners.co.uk/wp-content/uploads/2024/07/Group-1000005238.svg" alt="" loading="lazy" />
+                    <img
+                      src="/logo.png"
+                      alt="Proactive Cleaners Logo"
+                      className=" w-[260px] object-cover mr-10"
+                      style={{ height: '90px' }}
+                      loading="lazy"
+                    />
                   </Link>
                 </div>
               </div>
